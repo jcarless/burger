196588006d7089f0358384af9e447c81c5dfcaa6
Jerome Carless <carless.jerome@gmail.com>
2016-05-08 21:50:13 -0400

    working on orm

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO `burgerdb`.`burgers` (`BurgerName`, `Devour`, `Date`) VALUES ('KingBurger', '0', '1/1/2016');
+INSERT INTO `burgerdb`.`burgers` (`BurgerName`, `Devour`, `Date`) VALUES ('EverythingBurger', '0', '1/1/2016');
